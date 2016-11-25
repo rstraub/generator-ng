5d76b2a3f6d54c6d6fca80a411ca0b8edd978273
Roy <roy.straub1991@gmail.com>
2016-11-25 18:01:36 +0100

    COMP: Edited messages and added styles prompt

--- comp/index.js
+++ comp/index.js
@@ -6,22 +6,26 @@ var _ = require('lodash');
 module.exports = generators.Base.extend({
     constructor: function() {
         generators.Base.apply(this, arguments);
-        this.argument('name', { type: String, required: true });
+        this.argument('name', {type: String, required: true});
     },
     prompting: function() {
-        this.log(yosay('Let\'s build your ' + chalk.cyan(this.name) + ' Component'));
-
-        this.prompt([
-            {
-                type: 'confirm',
-                name: 'hasView',
-                message: 'Do you want a corresponding view?'
-            }
-        ]).then(function(answers) {
-            this.log(answers)
+        this.log(yosay('Let\'s cook up your ' + chalk.cyan(this.name) + ' Component'));
+        var done = this.async();
+        this.prompt([{
+            type: 'confirm',
+            name: 'hasView',
+            message: 'Do you want a view on the side ser?'
+        }, {
+            type: 'confirm',
+            name: 'hasStyles',
+            message: 'Some styling with that?'
+        }]).then(function(answers) {
+            this.hasView = answers.hasView;
+            this.hasStyles = answers.hasStyles;
+            done();
         }.bind(this));
     },
     writing: function() {
-
+        this.log(this.hasView, this.hasStyles);
     }
 });
